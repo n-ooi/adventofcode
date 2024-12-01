d7fb476aa31f8c15f539266a4992852d01413a12
Nathaniel Ooi <z5591783@ad.unsw.edu.au>
2024-12-01 23:49:32 +1100

    Day 1 solutions

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,4 +1,5 @@
 def main():
+    # Reading file
     with open("./day1.txt", "r") as file:
         lines = file.readlines()
 
@@ -13,19 +14,16 @@ def main():
     left_column.sort()
     right_column.sort()
 
+    # Star 1
     diffSum = 0;
-
     for i in range(len(left_column)):
         diffSum += abs(left_column[i] - right_column[i])
-
     print("Sum of differences:", diffSum);
 
-
+    # Star 2
     similarity = 0;
-
     for i in range(len(left_column)):
         similarity += left_column[i] * countOf(left_column[i], right_column)
-
     print("Similarity score:", similarity)
 
 def countOf(num, right):
