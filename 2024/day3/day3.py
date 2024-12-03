2dc706707a72e11d0bb20112c023b709cb6dfb42
Nathaniel Ooi <z5591783@ad.unsw.edu.au>
2024-12-03 16:39:03 +1100

    Day 3 solutions

---
+++ 2024/day3/day3.py
@@ -0,0 +1,40 @@
+import re
+
+def main():
+    with open("./day3.txt", "r") as file:
+        lines = file.read()
+
+    partOne(lines)
+    partTwo(lines)
+
+def partOne(lines):
+    # Define the regex pattern
+    pattern = r'mul\(\d+,\d+\)'
+    matches = re.findall(pattern, lines)
+
+    count = 0
+    for match in matches:
+        match = match[4:-1].split(",")
+        count += int(match[0]) * int(match[1])
+
+    print("Part One:", count)
+
+def partTwo(lines):
+    pattern = r'mul\(\d+,\d+\)|do\(\)|don\'t\(\)'
+    matches = re.findall(pattern, lines)
+
+    count = 0
+    enabled = True
+    for match in matches:
+        if match == "don't()":
+            enabled = False
+        elif match == "do()":
+            enabled = True
+        elif enabled == True:
+            match = match[4:-1].split(",")
+            count += int(match[0]) * int(match[1])
+
+    print("Part Two:", count)
+
+if __name__ == "__main__":
+    main()
